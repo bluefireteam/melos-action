5a0e8532b4e3ce776bb15081e1ae7a99ccb9b7d7
Luan Nico <luanpotter27@gmail.com>
2022-03-26 18:05:33 -0400

    Fix script so it will work on windows

--- setup.sh
+++ setup.sh
@@ -2,21 +2,20 @@
 version=$1
 run_bootstrap=$2
 
-echo Installing Melos $version
-
-if [ -x "$(command -v flutter)" ]
-then
-  flutter pub global activate melos $version
-elif [ -x "$(command -v dart)" ]
-then
-  dart pub global activate melos $version
+if [ -x "$(command -v flutter)" ]; then
+  exec=flutter
+elif [ -x "$(command -v dart)" ]; then
+  exec=dart
 else
   echo 'Error: Either dart or flutter needs to be on the PATH.' >&2
   exit 1
 fi
 
-if [ "$run_bootstrap" != "false" ]
-then
-  echo Running melos bootstrap
-  melos bootstrap
+echo Installing Melos $version via $exec
+
+$exec pub global activate melos $version
+
+if [ "$run_bootstrap" != "false" ]; then
+  echo 'Running melos bootstrap...'
+  $exec pub global run melos bootstrap
 fi

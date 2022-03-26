a7631f2ec854642697198b64768f1e6a66c22e21
Luan Nico <luanpotter27@gmail.com>
2022-03-26 18:51:56 -0400

    Address comments

--- setup.sh
+++ setup.sh
@@ -1,21 +1,13 @@
-#!/usr/bin/env bash
+#!/usr/bin/env bash -e
+
 version=$1
 run_bootstrap=$2
 
-if [ -x "$(command -v flutter)" ]; then
-  exec=flutter
-elif [ -x "$(command -v dart)" ]; then
-  exec=dart
-else
-  echo 'Error: Either dart or flutter needs to be on the PATH.' >&2
-  exit 1
-fi
+echo Installing Melos $version...
 
-echo Installing Melos $version via $exec
-
-$exec pub global activate melos $version
+dart pub global activate melos $version
 
 if [ "$run_bootstrap" != "false" ]; then
   echo 'Running melos bootstrap...'
-  $exec pub global run melos bootstrap
+  dart pub global run melos bootstrap
 fi
